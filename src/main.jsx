613848126b55cd4810b437f9bd66aab1cbcb561a
elianebenitez <elianenahilibenitezdiaz@gmail.com>
2026-04-18 10:12:59 +0200

    test: spa system running

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
 import App from './App.jsx'
+import Home from './pages/Home/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // App es el padre
+    children: [
+      {
+        index: true,    // Esto renderiza Home en la ruta raíz "/"
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    {/* Solo dejamos el RouterProvider. Él ya sabe que debe mostrar <App /> */}
+    <RouterProvider router={router} />
+  </StrictMode>
 )
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import styles from './Home.module.css'
+
+function Home() {
+     return(
+        <div>
+            <h1>Bienvenido a tu calculadora de la compra</h1>
+
+        </div>
+     )
+}
+
+export default Home;
